03dbaa9018cbb7dfdd688f5383c184f2b8876f56
LAPTOP-033RJKGR\jings <e0641717@u.nus.edu>
2020-09-01 18:20:27 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,9 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listcounter = 0;
+
         while(true) {
             Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
@@ -15,11 +16,20 @@ public class Duke {
             }
             else if(line.equals("list")){
                 for(int i=0;i<listcounter;i++){
-                    System.out.println((i+1) + ". " + list[i]);
+                    System.out.println((i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
                 }
             }
+            else if(line.contains("done")){
+                int a = Character.getNumericValue(line.charAt(5));
+                list[a-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:" + System.lineSeparator() + "[" +
+                        list[a-1].getStatusIcon() + "] " + list[a-1].description);
+
+            }
             else{
-                list[listcounter] = line;
+                Task t = new Task();
+                t.Task(line);
+                list[listcounter] = t;
                 System.out.println("added: " + line);
                 listcounter++;
             }
