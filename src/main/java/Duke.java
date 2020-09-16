fef447e22044efd0658f94f6d3b14dd9440ec845
LAPTOP-033RJKGR\jings <e0641717@u.nus.edu>
2020-09-16 15:34:34 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,81 +1,40 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) throws DukeException, IOException {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Task[] list = new Task[100];
-        int listcounter = 0;
+        TaskManager TM = new TaskManager();
 
         while(true){
             Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
+
             String[] action = line.split(" ", 2);
 
             if(action[0].equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
+                TM.refreshList();
                 break;
 
             } else if(action[0].equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for(int i=0;i<listcounter;i++){
-                    System.out.println((i + 1) + "." + list[i].toString());
-                }
+                TM.listTask();
 
             } else if(action[0].equals("done")){
-                try {
-                    int a = Integer.parseInt(action[1]);
-                    list[a - 1].markAsDone();
-                    System.out.println("Nice! I've marked this task as done:" + System.lineSeparator() + list[a - 1].toString());
-                } catch(ArrayIndexOutOfBoundsException e){
-                    System.out.println("Oops! The description of a done cannot be empty!");
-                }
+                TM.addDone(action[1]);
+
             } else if(action[0].equals("todo")) {
-                try {
-                    Todo td = new Todo(action[1]);
-                    list[listcounter] = td;
-                    System.out.println("Got it. I've added this task:" + System.lineSeparator() + list[listcounter].toString()
-                            + System.lineSeparator() + "Now you have " + (listcounter + 1) + " tasks in the list.");
-                    listcounter++;
-                } catch(ArrayIndexOutOfBoundsException e){
-                    System.out.println("Oops! The description of a todo cannot be empty!");
-                }
+                TM.addTodo(action[1]);
 
             } else if(action[0].equals("deadline")){
-                try {
-                    int slashIndex = action[1].indexOf("/");
-                    String deadlineBy = action[1].substring(slashIndex+1);
-                    Deadline dl = new Deadline(action[1].substring(0,slashIndex-1), deadlineBy);
-                    list[listcounter] = dl;
-                    System.out.println("Got it. I've added this task:" + System.lineSeparator() + list[listcounter].toString()
-                            + System.lineSeparator() + "Now you have " + (listcounter + 1) + " tasks in the list.");
-                    listcounter++;
-                } catch(ArrayIndexOutOfBoundsException e){
-                    System.out.println("Oops! The description of a deadline cannot be empty!");
-                }
+                TM.addDeadline(action[1]);
 
             } else if (action[0].equals("event")){
-                try {
-                    int slashIndex = action[1].indexOf("/");
-                    String eventBy = action[1].substring(slashIndex+1);
-                    Event e = new Event(action[1].substring(0, slashIndex-1), eventBy);
-                    list[listcounter] = e;
-                    System.out.println("Got it. I've added this task:" + System.lineSeparator() + list[listcounter].toString()
-                            + System.lineSeparator() + "Now you have " + (listcounter + 1) + " tasks in the list.");
-                    listcounter++;
-                } catch (ArrayIndexOutOfBoundsException e){
-                    System.out.println("Oops! The description of an event cannot be empty!");
-                }
+                TM.addEvent(action[1]);
 
             } else if (action[0].equals("delete")){
-                System.out.println("Noted. I've removed this task:");
-                int indexToDelete = Integer.parseInt(action[1]);
-                System.out.println(list[indexToDelete-1]);
-                listcounter--;
-                for(int j=(indexToDelete-1); j<listcounter; j++){
-                    list[j] = list[j+1];
-                }
-                System.out.println("Now you have " + listcounter + " tasks in the list.");
+                TM.deleteTask(action[1]);
             } else{
                 System.out.println("Oops! I'm sorry I have no idea what that means!");
             }
